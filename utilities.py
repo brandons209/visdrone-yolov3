78fb8a077db3f7850d6f7aa52ae6a2d90b20a56e
Brandon Silva <brandons209@aol.com>
2018-10-21 18:02:41 -0400

    network built from config file, working out bugs to model

---
+++ utilities.py
@@ -0,0 +1,78 @@
+from __future__ import division
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import cv2
+
+"""
+The output of YOLO is a convolutional feature map that contains the bounding box attributes along the depth of the feature map. The attributes bounding boxes
+predicted by a cell are stacked one by one along each other. So, if you have to access the second bounding of cell at (5,6), then you will have to index it by
+map[5,6, (5+C): 2*(5+C)]. This form is very inconvenient for output processing such as thresholding by a object confidence, adding grid offsets to centers,
+applying anchors etc.
+
+Another problem is that since detections happen at three scales, the dimensions of the prediction maps will be different. Although the dimensions of the three
+feature maps are different, the output processing operations to be done on them are similar. It would be nice to have to do these operations on a single tensor,
+rather than three separate tensors. So we use this function.
+"""
+def predict_transform(prediction, input_dim, anchors, num_classes, use_CUDA=True):
+    """
+    Takes a dection feature map and turns it into a 2-D tensor, with each row corresponding to attributes of a bounding box in this order:
+    1 B.Box at (0,0)
+    2 B.Box at (0,0)
+    3 B.Box at (0,0)
+
+    1 B.Box at (0,1)
+    2 B.Box at (0,1)
+    3 B.Box at (0,1)
+    ...
+    ...
+    """
+    batch_size = prediction.size(0)
+    stride = input_dim // prediction.size(2)
+    grid_size = input_dim // stride
+    bbox_attr = 5 + num_classes
+    num_anchors = len(anchors)
+
+    if use_CUDA:
+        prediction = prediction.cuda()
+
+    prediction = prediction.view(batch_size, bbox_attr*num_anchors, grid_size**2)
+    prediction = prediction.transpose(1,2).contiguous()
+    prediction = prediction.view(batch_size, num_anchors*grid_size**2, bbox_attr)
+    #input image is larger by a factor of stride variable than detection map, so we divide anchors by the stride of the detection feature map
+    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
+    #run sigmoid on x,y coords and objectness score
+    prediction[:,:,0] = torch.sigmoid(prediction[:,:,0])
+    prediction[:,:,1] = torch.sigmoid(prediction[:,:,1])
+    prediction[:,:,4] = torch.sigmoid(prediction[:,:,4])
+    #add grid offsets to center coordinates prediction
+    grid = np.arange(grid_size)
+    x,y = np.meshgrid(grid, grid)
+
+    x_offset = torch.FloatTensor(x).view(-1,1)
+    y_offset = torch.FloatTensor(y).view(-1,1)
+
+    if use_CUDA:
+        x_offset = x_offset.cuda()
+        y_offset = y_offset.cuda()
+
+    x_y_offset = torch.cat( (x_offset, y_offset), 1).repeat(1, num_anchors).view(-1,2).unsqueeze(0)
+    prediction[:,:,:2] += x_y_offset
+
+    #apply anchors to dimensions of bounding box using log space transform of height and width
+    anchors = torch.FloatTensor(anchors)
+
+    if use_CUDA:
+        anchors = anchors.cuda()
+
+    anchors = anchors.repeat(grid_size**2, 1).unsqueeze(0)
+    prediction[:,:,2:4] = torch.exp(prediction[:,:,2:4])*anchors
+    #apply sigmoid to class scores
+    prediction[:,:,5: 5 + num_classes] = torch.sigmoid((prediction[:,:, 5 : 5 + num_classes]))
+
+    #resize detections map to size of input image by multiplying by stride
+    prediction[:,:,:4] *= stride
+    return prediction
--- yolo.py
+++ yolo.py
@@ -5,54 +5,71 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+import yolo_parser
+import utilities as util
+import cv2
 
-def parse_yolo_cfg(path):
-    """
-    Takes a yolo cfg file and extracts layer information from it.
-    """
-    with open(path, 'r') as file:
-        lines = file.read().split('\n')
-        lines = [non_empty for non_empty in lines if len(non_empty) > 0] #remove blank lines
-        lines = [non_comment for non_comment in lines if non_comment[0] != '#'] # remove comments
-        lines = [line.rstrip().lstrip() for line in lines] #remove leading and trailing whitespaces from each line
+class YoloNet(nn.Module):
+    def __init__(self, yolo_cfg_file):
+        super(YoloNet, self).__init__()
+        self.blocks = yolo_parser.parse_yolo_cfg(yolo_cfg_file)
+        self.network_info, self.module_list = yolo_parser.build_py_modules(self.blocks)
 
-    network_block = {}
-    network = []
+    def forward(self, input, use_CUDA=True):
+        modules = self.blocks[1:] #exclude the first block, which is a net block that just contains network information
+        outputs = {} #cache outputs of each layer for the route and shortcut layers, as they require features maps from previous layers
 
-    for line in lines:
-        if line[0] == "[":#start of new block
-            if len(network_block) != 0:#if block is non empty, it contains a previous block
-                network.append(network_block) #append block to network, reset block variable
-                network_block = {}
-            block["type"] = line[1:-1].rstrip()
-        else:
-            key, value = line.split("=")
-            block[key.rstrip()] = value.lstrip()
-    network.append(network_block)
+        write = 0#flag used to indicate whether the first detection occured or not, used to determine whether to concatenate dection maps or not
+        for i, module in enumerate(modules):
+            module_type = module["type"]
 
-    return network
+            if module_type == "convolutional" or module_type == "upsample":
+                input = self.module_list[i](input)
+            elif module_type == "route":
+                layers = module["layers"]
+                layers = [int(x) for x in layers]
 
-def build_py_modules(network):
-    """
-    Takes a list of modules from a yolo cfg file and builds pytorch modules for them. Since pytorch only has
-    modules for convolution and upsampling, custom modules need to be made for the route, skip, downsampleing layers.
-    """
-    network_info = network[0]
-    module_list = nn.ModuleList()
-    prev_block_filters = 3
-    output_filters = []
+                if layers[0] > 0:
+                    layers[0] = layers[0] - i
+                if len(layers) == 1:
+                    input = outputs[i + layers[0]]
+                else:
+                    if layers[1] > 0:
+                        layers[1] = layers[1] - i
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+                    #concatenate two feature maps we use the torch.cat function with the second argument as 1 to concatenate along the depth
+                    input = torch.cat( (map1, map2), 1)
+            elif module_type == "shortcut":
+                from_layer = int(module["from"])
+                input = outputs[i-1] + outputs[i+from_layer]
+            elif module_type == "yolo":
+                anchors = self.module_list[i][0].anchors
+                input_dim = int(self.network_info["height"])
+                num_classes = int(module["classes"])
 
-    for index, block in enumerate(blocks[1:]):#iterate through blocks, create a module for each block
-        #since blocks have multiple layers, like activation and normalization, use sequential to build modules
-        module = nn.Sequential()
-        if block["type"] == "convolutional":
-            activation = block["activation"]
-            try:#check for batch normalization layer
-                batch_normalize = int(x["batch_normalize"])
-                bias = False
-            except:
-                batch_normalize = 0
-                bias = True
+                input = input.data
+                input = util.predict_transform(input, input_dim, anchors, num_classes, use_CUDA)
+                if not write:#no collecter initalized
+                    detections = input
+                    write = 1
+                else:#concatenate feature maps to collecter
+                    detections = torch.cat((detections, input), 1)
 
-            num_filters = int(block["filters"])
-            
+            outputs[i] = input
+        return detections
+
+
+def get_test_input():
+    img = cv2.imread("dog-cycle-car.png")
+    img = cv2.resize(img, (608,608))          #Resize to the input dimension
+    img_ =  img[:,:,::-1].transpose((2,0,1))  # BGR -> RGB | H X W C -> C X H X W
+    img_ = img_[np.newaxis,:,:,:]/255.0       #Add a channel at 0 (for batch) | Normalise
+    img_ = torch.from_numpy(img_).float()     #Convert to float
+    img_ = Variable(img_)                     # Convert to Variable
+    return img_
+
+model = YoloNet("yolov3.cfg")
+input = get_test_input()
+pred = model(input, torch.cuda.is_available())
+print(pred)
