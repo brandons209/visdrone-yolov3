9037ef2ab6b5726d9697520c64b94dae98ea73aa
Brandon Silva <brandons209@aol.com>
2018-10-28 22:38:06 -0400

    getting there for training

--- train_visdrone.py
+++ train_visdrone.py
@@ -23,5 +23,16 @@ input_dim = (608,608)
 original_img_dim = (1920,1080)
 
 train_images_paths, train_anno = glob.glob("data/train/images/*"), util.load_annotations("data/train/annotations/", input_dim, original_img_dim)
-
 train_images = [util.prepare_image(cv2.imread(img), input_dim) for img in train_images_paths]
+valid_images_paths, valid_anno = glob.glob("data/valid/images/*"), util.load_annotations("data/train/annotations", input_dim, original_img_dim)
+valid_images = [util.prepare_image(cv2.imread(img), input_dim) for img in valid_images_paths]
+
+epochs = 2
+lr = 0.001
+momentum = 0.9
+
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+loss_layers = model.loss_layers
+for l in loss_layers:
+    l.seen = model.seen
